0f87a250e7cdd245c55e9f283b67d7f9017b467f
RUTVIK MHASKE <rutvikmhaske720@gmail.com>
2026-07-26 20:48:35 +0530

    Time: 0 ms (100%), Space: 8.2 MB (83.18%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int absent = 0;
+        int late = 0;
+        
+        for (char c : s) {
+            if (c == 'A') {
+                absent++;
+                if (absent >= 2) return false;
+                late = 0; 
+            } else if (c == 'L') {
+                late++;
+                if (late >= 3) return false;
+            } else {
+               
+                late = 0; 
+            }
+        }
+        
+        return true;
+    }
+};
