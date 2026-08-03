539f3bdb239077455c9e8fc4cee9d49bc5a82473
RUTVIK MHASKE <rutvikmhaske720@gmail.com>
2026-08-03 19:23:13 +0530

    Time: 16 ms (78.93%), Space: 136.1 MB (76.04%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        
+        vector<int> dp(n + 1, -1e9); 
+        
+        dp[n] = 0;
+        
+        for (int i = n - 1; i >= 0; --i) {
+            int take = 0;
+            for (int j = 0; j < 3 && i + j < n; ++j) {
+                take += stoneValue[i + j];
+                dp[i] = max(dp[i], take - dp[i + j + 1]);
+            }
+        }
+        
+        if (dp[0] > 0) return "Alice";
+        if (dp[0] < 0) return "Bob";
+        return "Tie";
+    }
+};
